6bd8dcb1a1059dbab0f9023b52c251f8ed872d73
Sara Martinez <s_martinez_1990@outlook.com>
2020-09-11 23:41:37 -0400

    conditions working for single room

--- Program.cs
+++ Program.cs
@@ -12,11 +12,24 @@ namespace Hotel
             var roomType = Int32.Parse(Console.ReadLine());
             if(roomType == 1) { 
                 Console.WriteLine("you chose 1");
-
+                Console.WriteLine("Please select your floor preference (1-12)");
+                var floorNumber = Int32.Parse(Console.ReadLine());
+                if (floorNumber <= 5 && roomType == 1){
+                    Console.WriteLine("Chose room # from 1 - 5 Cost is 45.00");
+                }else if (floorNumber == 12) {
+                    Console.WriteLine("We are sorry, a single bedroom is not available in this floor");
+                }else {
+                    Console.WriteLine("Chose floor # 6 - 11 cost 55.00");
+                }
+        //THIS IS WHERE I LEFT OFF - WRITING CONDITIONS FOR THE ROOM TYPE TWO COSTS. 
             }else if (roomType == 2) {
                 Console.WriteLine("you chose 2");
+                Console.WriteLine("Please select your floor preference (1-12)");
+
             }else if (roomType == 3) {
                 Console.WriteLine("you chose 3");
+                Console.WriteLine("Please select your floor preference (1-12)");
+
             } else{
                 Console.WriteLine("invalid choise");
             }
